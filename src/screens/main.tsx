3e4842ff570216064e3a0a29d771ae17c4411793
DonRehan <30264386+donRehan@users.noreply.github.com>
2023-11-30 21:05:57 +0200

    Fix app not running on iphone
    Updated react component versions and yarn start now runs the app on
    Iphone linux mint.
    [Practice Commit messages]

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -8,7 +8,8 @@ export default function MainScreen() {
 	return (
 		<Center _dark= {{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} px={4} flex={1}>
 		<VStack space={5} alignItems="center">
-		<Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}><Text>Hello</Text></Box>
+		<Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}><Text>Hello</Text>
+		</Box>
 		<ThemeToggle />
 		</VStack>
 		</Center>
